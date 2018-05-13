c7bab3ea551e6c0d775d618d29b5a4392b1682d4
unknown <sonnguyend10s@gmail.com>
2018-05-14 01:57:37 +0700

    custom search view, prepare js file

---
+++ public/js/article.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+  $('#search-bar').on('input',function(e){
+    var value = $('#search-bar').val();
+    
+  });
+});
--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -8,7 +8,6 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Search Articles</title>
-
     <script src="{{ asset('/js/article.js') }}"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
@@ -16,29 +15,16 @@
   <body>
     <div class="container search-container">
       <h4>Search Results</h4>
+      <input type="text" class="search-bar" id="search-bar"/>
       <table class="table table-striped">
         <thead>
           <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Email</th>
+            <th>Article</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-          </tr>
-          <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
-          </tr>
-          <tr>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
           </tr>
         </tbody>
       </table>
